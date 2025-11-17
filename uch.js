54447a9f99e73693a8f3fbb953c42c15242b2cfa
Ayubxon Xamidov <ayyuubxonxamidov@gmail.com>
2025-11-17 08:18:36 +0500

    Exam

---
+++ uch.js
@@ -0,0 +1,102 @@
+let header = document.querySelector("header")
+let div1 = document.querySelector(".div1")
+let div2 = document.querySelector(".div2")
+let ah = document.querySelector(".div2 .ah")
+
+
+header.style.display = "flex"
+header.style.justifyContent = "space-around"
+header.style.alignItems = "center"
+div2.style.display = "flex"
+div2.style.gap = "50px"
+
+//divin
+let divin = document.querySelector(".divin")
+let div3 = document.querySelector(".div3")
+let div4 = document.querySelector(".div4")
+let img1 = document.querySelector(".img1")
+let btn1 = document.querySelector(".btn1")
+let katta = document.querySelector(".katta")
+let img2 = document.querySelector(".img2")
+
+//
+
+//divin1
+let divin1 = document.querySelector(".divin1")
+let div5 = document.querySelector(".div5")
+let div6 = document.querySelector(".div6")
+let img3 = document.querySelector(".img3")
+let btn2 = document.querySelector(".btn2")
+let img4 = document.querySelector(".img4")
+//
+
+//divin2
+let divin2 = document.querySelector(".divin2")
+let div7 = document.querySelector(".div7")
+let div8 = document.querySelector(".div8")
+let btn3 = document.querySelector(".btn3")
+//
+
+//divin3
+let divin3 = document.querySelector(".divin3")
+let div9 = document.querySelector(".div9")
+let div10 = document.querySelector(".div10")
+let img5 =document.querySelector(".img5")
+//
+
+katta.style.backgroundColor = "yellow"
+katta.style.height = "500px"
+
+
+// divin
+
+divin.style.display = "flex"
+divin.style.justifyContent = "space-around"
+header.style.color = "white"
+img1.style.width = "250px"
+img1.style.height = "250px"
+divin.style.alignItems = "center"
+divin.style.marginTop = "70px"
+btn1.style.marginTop = "50px"
+btn1.style.backgroundColor = "chocolate"
+btn1.style.borderColor = "chocolate"
+divin.style.marginTop = "30px"
+divin.style.color = "white"
+img2.style.marginTop = "70px"
+img2.style.width = "900px"
+img2.style.marginLeft = "85px"
+
+
+//divin2
+divin1.style.display = "flex"
+divin1.style.justifyContent = "space-around"
+img3.style.height = "413px"
+divin1.style.alignItems = "center"
+divin1.style.backgroundColor = "chocolate"
+divin1.style.width = "900px"
+divin1.style.marginLeft = "85px"
+divin1.style.marginTop = "80px"
+btn2.style.backgroundColor = "black"
+btn2.style.color = "white"
+btn2.style.width = "130px"
+btn2.style.height = "35px"
+btn2.style.borderColor = "black"
+img4.style.width = "900px"
+img4.style.marginTop = "50px"
+img4.style.marginLeft = "85px"
+divin2.style.display = "flex"
+divin2.style.gap = "20px"
+divin2.style.marginLeft = "85px"
+divin2.style.marginTop = "40px"
+divin2.style.alignItems = "center"
+div8.style.width = "440px"
+div8.style.height = "258px"
+div8.style.backgroundColor = "rgb(224, 224, 224)"
+divin3.style.display = "flex"
+divin3.style.alignItems = "center"
+divin3.style.gap = "20px"
+divin3.style.marginLeft = "85px"
+img5.style.width = "440px"
+img5.style.height = "450px"
+div9.style.width = "440px"
+divin3.style.marginTop = "80px"
